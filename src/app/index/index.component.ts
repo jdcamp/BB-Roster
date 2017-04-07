e05abf6510e987a3bcbcf22804c186144485dc4a
jdc <jdcxxx@gmail.com>
2017-04-07 11:03:04 -0700

    create player service

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { Player } from '../player.model';
+import { PlayerService } from '../player.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
-  styleUrls: ['./index.component.css']
+  styleUrls: ['./index.component.css'],
+  providers: [PlayerService]
 })
 export class IndexComponent implements OnInit {
-
-  constructor() { }
+  players: FirebaseListObservable<any[]>;
+  constructor(private router: Router, private playerService: PlayerService){}
 
   ngOnInit() {
+      this.players = this.playerService.getPlayers();
   }
 
 }
