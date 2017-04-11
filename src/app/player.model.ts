ef1e55891785f581efa598915febd91b3ab43066
jdc <jdcxxx@gmail.com>
2017-04-11 00:29:50 -0700

    add img links to player class, display images in player-list

--- src/app/player.model.ts
+++ src/app/player.model.ts
@@ -1,3 +1,3 @@
 export class Player {
-    constructor(public name: string, public position: string, public playerNumber: number) { }
+    constructor(public name: string, public position: string, public playerNumber: number, public img = 'http://placehold.it/100x100') { }
 }
